e1fd7c04fd419a26d46e8f7657f2d2dce58693e7
Elia Schito <elia@schito.me>
2020-06-23 19:07:32 +0200

    elia/rely-on-sdk-request-classes: [wip]

--- spec/features/backend/new_payment_method_spec.rb
+++ spec/features/backend/new_payment_method_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe "creating a new payment" do
     visit "/admin/payment_methods/new"
     expect(page).to have_select('payment_method_type', :options => ["Paypal Commerce Platform", "Bogus Credit Card Payments", "Check Payments", "Simple Bogus Credit Card Payments", "Store Credit Payments"])
   end
+
+  scenario "should display the onboarding button", :js do
+    visit "/admin/payment_methods"
+
+    main_window = current_window
+    paypal_window = window_opened_by do
+      within ".setup-wizard" do
+        paypal_button = page.find("button")
+        expect(paypal_button.text).to eq("Setup PayPal Commerce Platform")
+        pending "TODO: make this more complete"
+        # paypal_button.click
+      end
+    end
+  end
 end
